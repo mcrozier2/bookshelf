fae4f480d2789e31e81e70c88f894a556d43371e
mcrozier <mcrozier2@gmail.com>
2022-01-15 15:00:26 -0600

    Exercise 1 - extra credit 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,20 +1,38 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {Logo} from 'components/logo'
+import {Logo} from './components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 function App() {
-    return (
-        <div>  
-            <Logo width="80" height="80" />
-            <h1>Bookshelf</h1>
-            <div>
-                <button onClick={() => alert('login clicked')}>Login</button>
-            </div>
-            <div>
-                <button onClick={() => alert('register clicked')}>Register</button>
-            </div>
+  const [openDialog, setOpenDialog] = React.useState('none');
+
+  const closeDialog = () => setOpenDialog('none');
+
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <div>
+        <button onClick={() => setOpenDialog('login')}>Login</button>
+      </div>
+      <div>
+        <button onClick={() => setOpenDialog('register')}>Register</button>
+      </div>
+      <Dialog aria-label="Login form" isOpen={openDialog === 'login'}>
+        <div>
+          <button onClick={closeDialog}>close</button>
         </div>
-    )
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog aria-label="Registration form" isOpen={openDialog === 'register'}>
+        <div>
+          <button onClick={closeDialog}>close</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
