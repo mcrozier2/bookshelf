e4e8e4230df540244f73b005dca07ac4aa60457e
mcrozier <mcrozier2@gmail.com>
2022-01-16 12:03:06 -0600

    Exercise 3 - extra credit 1

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -4,7 +4,14 @@ function client(endpoint, customConfig = {}) {
     ...customConfig
   }
   return fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
-  .then(response => response.json());
+    .then(async response => {
+      const data = await response.json();
+      if(response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
 }
 
 export {client}
