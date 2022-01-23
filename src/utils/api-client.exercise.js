dacd8fabff9a4063cba99ad5f1a55dd494437838
mcrozier <mcrozier2@gmail.com>
2022-01-23 16:01:29 -0600

    Exercise 4 - extra credit 3

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,4 +1,5 @@
-const apiURL = process.env.REACT_APP_API_URL
+import * as auth from 'auth-provider';
+const apiURL = process.env.REACT_APP_API_URL;
 
 function client(endpoint, {token, headers: customHeaders, ...customConfig} = {}) {
   const config = {
@@ -11,6 +12,11 @@ function client(endpoint, {token, headers: customHeaders, ...customConfig} = {})
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if(response.status === 401) {
+      auth.logout();
+      window.location.assign(window.location);
+      return Promise.reject({message: 'Please re-authenticate'});
+    }
     const data = await response.json()
     if (response.ok) {
       return data
